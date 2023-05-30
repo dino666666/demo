be30b92510a05b789f92d5414a60a4edb4d9c50c
zhanghan <zhanghanyxx@163.com>
2023-05-30 22:41:22 +0800

    1

--- debug.py
+++ debug.py
@@ -5,6 +5,8 @@ import pytest
 
 if __name__ == "__main__":
      pytest.main(['-s', '-v', 'jenkins_study/test_demo.py', '-q', '--alluredir', './allure-results'])
+     import time
+     time.sleep(5)
      # import os
      # _out = os.popen("allure generate allure-results -o allure_reports/ --clean").read()
      # print(_out)
