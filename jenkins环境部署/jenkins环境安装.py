ebd0c775e20eee839b7ace7cb68f4ab7e20564e5
zhanghan <zhanghan@163.com>
2023-06-07 23:14:00 +0800

    1

---
+++ jenkins环境部署/jenkins环境安装.py
@@ -0,0 +1,68 @@
+'''
+0.安装Java
+    -sudo apt install openjdk-11-jdk
+
+1.导入密钥： 使用wget导入jenkins存储库密钥GPG
+    -wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+2.添加存储库： 将jenkins存储库添加到系统中
+    -sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+3.安装
+    -sudo apt update
+    -sudo apt install jenkins
+
+4.切换jenkins用户
+    -zhanghan@zhanghan-virtual-machine:~$ sudo chmod 777 /etc/default/jenkins
+    -zhanghan@zhanghan-virtual-machine:~$ sudo vim /etc/default/jenkins
+        JENKINS_USER=zhanghan
+        JENKINS_GROUP=zhanghan
+    -zhanghan@zhanghan-virtual-machine:~$ sudo chmod 777 /usr/lib/systemd/system/jenkins.service
+    -zhanghan@zhanghan-virtual-machine:~$ vim /usr/lib/systemd/system/jenkins.service
+        User=zhanghan
+        Group=zhanghan
+    -zhanghan@zhanghan-virtual-machine:~$ sudo systemctl daemon-reload
+        选择zhanghan用户
+    -zhanghan@zhanghan-virtual-machine:~$ sudo chown -R zhanghan:zhanghan /var/lib/jenkins
+    -zhanghan@zhanghan-virtual-machine:~$ sudo chown -R zhanghan:zhanghan /var/cache/jenkins
+    -zhanghan@zhanghan-virtual-machine:~$ sudo chown -R zhanghan:zhanghan /var/log/jenkins
+    -zhanghan@zhanghan-virtual-machine:~$ sudo systemctl restart jenkins
+
+4.检查Jenkins运行状态
+    -sudo systemctl status jenkins
+    -sudo systemctl restart jenkins   //重启
+
+5.打开Jenkins
+    -http://192.168.10.107:8080/ 1
+
+
+
+7.Jenkins第一次登录密码
+    - sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+6.Jenkins配置
+    - [可选]开放端口： sudo ufw allow 8080
+    - [可选]修改端口： vi /etc/default/jenkins  修改HTTP_PORT
+    -[可选]修改jenkins工作目录：
+        -修改：vi /var/lib/jenkins/config.xml  修改workspaceDir
+        -工作目录：ls -l /var/lib/jenkins/workspace/
+    -[可选]JENKINS_HOME在哪里：/var/lib/jenkins
+    -[可选]jenkins日志文件： cat /var/log/jenkins/
+    -[可选]jenkins配置文件：/etc/default/jenkins
+
+
+问题一：sudo apt update时报 “由于没有公钥，无法验证下列签名： NO_PUBKEY 5BA31D57EF5975CA” 错误
+解决：sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA（密钥需要按照问题的提示更换一样的）
+
+问题二：sudo apt install jenkins报“Sub-process /usr/bin/dpkg returned an error code (1)”错误
+解决：
+    cd /var/lib/dpkg/
+    sudo mv info/ info_bak          # 现将info文件夹更名
+    sudo mkdir info                 # 再新建一个新的info文件夹
+    sudo apt-get update             # 更新
+    sudo apt-get -f install         # 修复
+    sudo mv info/* info_bak/        # 执行完上一步操作后会在新的info文件夹下生成一些文件，现将这些文件全部移到info_bak文件夹下
+    sudo rm -rf info                # 把自己新建的info文件夹删掉
+    sudo mv info_bak info           # 把以前的info文件夹重新改回名
+
+'''
---
+++ pipeline/pipeline_第一个html邮件格式.py
@@ -0,0 +1,82 @@
+'''
+
+
+1.pipeline脚本：
+    pipeline {
+        agent any
+        stages {
+            stage('Hello') {
+                steps {
+                    echo 'Hello, Jenkins!'
+                }
+            }
+        }
+        post {
+            always {
+                emailext (
+                    subject: '${DEFAULT_SUBJECT}',
+                    body: '${DEFAULT_CONTENT}',
+                    to: '798255210@qq.com'
+                )
+            }
+        }
+    }
+
+2.DEFAULT_CONTENT内容：
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Title</title>
+            <style type="text/css">
+                .title{
+                    text-align: center;
+                    color: rgb(60, 60, 60);
+                }
+                .desc{
+                    text-align: left;
+                }
+            </style>
+        </head>
+        <body leftmargin="8" marginwidth="0" topmargin="8" marginheight="4" offset="0">
+        <table width="95%" cellpadding="0" cellspacing="0" style="font-size: 16pt; font-family: Tahoma, Arial, Helvetica, sans-serif">
+            <div>
+                <h1 class="title">稳定性自动化测试报告</h1>
+            </div>
+            <div class="desc">
+                <p><font color="red">Jenkins自动发送的测试报告邮件，无需回复！</font></p>
+                <h2>在线测试报告直达链接：<a href="${PROJECT_URL}${BUILD_NUMBER}/allure">${PROJECT_URL}${BUILD_NUMBER}/allure</a></h2>
+            </div>
+            <tr>
+                <td><br />
+                <b><font color="#0B610B"><font size="6">构建信息</font></font></b>
+                <hr size="2" width="100%" align="center" /></td>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                    <div style="font-size:18px">
+                        <li>构建名称：${PROJECT_NAME}</li>
+                        <li>构建结果: <span style="color:green"> Successful </span></li>
+                        <li>构建编号：${BUILD_NUMBER}</li>
+                        <li>触发原因：${CAUSE}</li>
+                        <li>部署分支：None </li>
+                        <li>测试报告地址：<a href="${PROJECT_URL}${BUILD_NUMBER}/allure">${PROJECT_URL}${BUILD_NUMBER}/allure</a></li>
+                        <li>环境: None </li>
+                        <li>测试阶段：None </li>
+                        <li>变更概要：None </li>
+                        <li>变更集: None</li>
+                        <li>构建地址：<a href=${BUILD_URL}>${BUILD_URL}</a></li>
+                        <li>构建日志：<a href=${BUILD_URL}console>${BUILD_URL}console</a></li>
+                    </div>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        </body>
+        </html>
+
+
+
+
+'''
